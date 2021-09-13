0e2eb957d1a3527a0333caf26cf5cd528aca22fa
takashiono2 <takashiono710@gmail.com>
2021-09-13 15:27:36 +0900

    chapter8

--- chapter7/my-cli/src/App.vue
+++ chapter7/my-cli/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <img alt="vue logo" src="./assets/logo.png">
-    <HelloWorld mgs="Welcom ton Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
     <p>こんにちは、Vue.js！</p>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+<script >
+// export default {
+//   name: 'app',
+//   components: {
+//   }
+// }
 </script>
 
-<style>
+<style scoped>
+p{
+  border: 1px solid Red;
+  background-color:Yellow;
+}
+</style>
