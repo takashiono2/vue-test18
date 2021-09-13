b0f562bc20d33a49e142c6711370b49e914c8786
takashiono2 <takashiono710@gmail.com>
2021-09-13 13:42:48 +0900

    コミット2回目

---
+++ chapter7/my-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="vue logo" src="./assets/logo.png">
+    <HelloWorld mgs="Welcom ton Your Vue.js App"/>
+    <p>こんにちは、Vue.js！</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
