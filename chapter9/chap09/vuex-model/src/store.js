e25c193c15e0f886850dd010cdef7839725775d2
takashiono2 <takashiono710@gmail.com>
2021-09-20 00:21:07 +0900

    chapter9の9-2-1

--- chapter9/chap09/vuex-model/src/store.js
+++ chapter9/chap09/vuex-model/src/store.js
@@ -4,12 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
+    name: ''
   },
   mutations: {
+    updateName(state, name) {
+      state.name = name
+    }
   },
+  
   actions: {
-  },
-  modules: {
+
   }
 })
--- chapter9/chap09test/src/App.vue
+++ chapter9/chap09test/src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <input type="button" value="-" @click="minus" />
+    {{count}}
+    <input type="button" value="+" @click="plus" />
   </div>
 </template>
 
+<script>
+export default{
+  name: 'app',
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods:{
+    minus(){
+      this.$store.commit('minus')
+    },
+    plus(){
+      this.$store.commit('plus')
+    }
+   }
+  }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
